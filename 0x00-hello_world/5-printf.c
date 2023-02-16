832d5175371fd674dd8514fedb6e40b3255df262
Peace <mensahjmpeace@yahoo.com>
2023-02-16 14:53:03 -0800

    my second c line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main- is the entry point of this tak, print a C program using printf
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
