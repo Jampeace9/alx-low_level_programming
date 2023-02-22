5bfdf23bab2d800a07fd7cb0ae1730e2e28ef3c2
Peace <mensahjmpeace@yahoo.com>
2023-02-21 16:45:45 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - prints _putchar
+ *
+ * Return: Always 0 in success
+ */
+int main(void)
+{
+	_putchar(95); /*95 is asci for _*/
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
