7918226a7a1d7d0b2cc13b7c782c830be4f1732d
Peace <mensahjmpeace@yahoo.com>
2023-02-23 19:23:03 -0800

    putchar

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,6 +4,7 @@
  * This is like creating my own library with declared functions
  * The tasks defines  the functions
  */
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
