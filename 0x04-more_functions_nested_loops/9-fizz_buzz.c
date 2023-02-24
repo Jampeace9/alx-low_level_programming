79499d19d5cdf6cfff67916b6359c1d2decff7ad
Peace <mensahjmpeace@yahoo.com>
2023-02-23 20:10:52 -0800

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Almighty Fizzbuzz, prints numbers from 1 to 100,
+ *        but any numbers which is multiple of 3, print Fizz,
+ *        and any numbers which is multiple of 5 print Buzz,
+ *        and numbers both multiple of 3 & 5,print FizzBuzz.
+ *
+ * Return: 0 if true.
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 1; number <= 100; number++)
+	{
+		if (number % 3 == 0 && number % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (number % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (number % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else
+		{
+			printf("%d", number);
+		}
+		if (number != 100)
+		{
+		printf(" ");
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
